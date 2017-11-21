e380d83f23d2e9a8f1e2a7520eec2a5ad97a1bf4
Codigos-de-Guerra <32881985+Codigos-de-Guerra@users.noreply.github.com>
2017-11-20 22:40:43 -0300

    Update processos.h

--- processos.h
+++ processos.h
@@ -3,6 +3,8 @@
 
 #include "uteis.h"
 
+#define PI 3.1415926535
+
 Imagem* make_PPM_cinza (Imagem *m);
 
 Imagem *GaussFilter (Imagem *m, int blurtimes);
@@ -10,5 +12,7 @@ Imagem *GaussFilter (Imagem *m, int blurtimes);
 Imagem *SobelFilter (Imagem *m, int limite);
 
 Imagem *Binarizacao (Imagem *m, int limiar);
+
+Centro *Transformada_Hough (Imagem *m);
 
 #endif

254915bf63af0bc18218751e4c0a2c496e20ed67
Codigos-de-Guerra <32881985+Codigos-de-Guerra@users.noreply.github.com>
2017-11-24 17:35:08 -0300

    QUASEQUASE

--- write.h
+++ write.h
@@ -3,6 +3,7 @@
 
 #include "uteis.h"
 
+//Funcao de escrita da imagem passada por parametro, em um arquivo cujo nome tambem e passado por parametro
 void write_img(Imagem *m, char *outfilename);
 
 #endif
